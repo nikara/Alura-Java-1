af84d6a86222656be82faf9b27a7462842cc9a43
Nikara <nickolaslamin@gmail.com>
2025-12-09 15:26:11 -0300

    Segundo comite

---
+++ Java1/src/modulo1/texto.java
@@ -0,0 +1,29 @@
+package modulo1;
+
+public class texto {
+
+	public static void main(String[] args) {
+
+		/*
+		 * Em Java, a classe String é uma das mais importantes e utilizadas. Ela é
+		 * responsável por representar uma sequência de caracteres, ou seja, um texto.
+		 */
+
+		/* Criação de Strings */
+		// Para criar uma String em Java, basta utilizar aspas duplas "" para delimitar
+		// o texto. Por exemplo:
+
+		String nome = "Nickolas";
+
+		/* Nesse exemplo, a variável nome recebe uma String com o texto "Nickolas". 
+		 * É possível concatenar duas ou mais Strings utilizando o operador+, 
+		 * por exemplo*/
+		
+		String saudacao = "Olá";
+		String mensagem = saudacao + nome +"!";
+		
+		
+		
+
+	}
+}
